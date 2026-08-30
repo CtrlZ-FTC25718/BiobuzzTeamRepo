2a0929495a5e091fb70e7824a013ca659cc34e30
CtrlZ-FTC25718 <ftc.ctrlz.25718@gmail.com>
2026-08-30 13:42:16 -0400

    feat: added pedro-pathing package

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/ShooterPIDTuning.java
@@ -0,0 +1,223 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import android.util.Log;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.HeadingInterpolator;
+import com.pedropathing.paths.Path;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Intake;
+import org.firstinspires.ftc.teamcode.Shooter;
+import org.firstinspires.ftc.teamcode.Sorter;
+import org.firstinspires.ftc.teamcode.pedroPathing.constants.Constants;
+
+import java.util.function.Supplier;
+
+@Configurable
+@Disabled
+@TeleOp(name = "ShooterPIDTuning")
+public class ShooterPIDTuning extends OpMode {
+
+    private TelemetryManager telemetryM;
+    private Sorter sorter;
+    private Intake intake;
+    private Shooter shooter;
+    private DcMotorEx front;
+    private DcMotorEx back;
+
+    private double highVelocity = 2000;
+    private double lowVelocity = 0;
+
+    private double targetVelocity = lowVelocity;
+
+    private double currFrontTargetVelocity = lowVelocity;
+    private double currBackTargetVelocity = lowVelocity;
+
+    private double frontF = 16.4;//15.106
+    private double frontP = 60;//46.601, then 300
+    private double frontI = 0;
+    private double frontD = 0;
+    private double backF = 20.6;
+    private double backP = 50;
+    private double backI = 0;
+    private double backD = 0;
+
+    private PIDFCoefficients frontPIDF;
+    private PIDFCoefficients backPIDF;
+
+    double[] stepSizes = {10.0, 1.0, 0.1, 0.001, 0.0001};
+    int stepIndex = 1;
+
+    /** This method is call once when init is played, it initializes the follower **/
+    @Override
+    public void init() {
+        intake = new Intake(hardwareMap);
+        sorter = new Sorter(hardwareMap);
+        shooter = new Shooter(hardwareMap);
+
+        front = shooter.getShooterFrontMotor();
+        back = shooter.getShooterBackMotor();
+
+        frontPIDF = new PIDFCoefficients(frontP,frontI,frontD,frontF);
+        backPIDF = new PIDFCoefficients(backP,backI,backD,backF);
+
+        front.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, frontPIDF);
+        back.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, backPIDF);
+    }
+
+    /** This method is called continuously after Init while waiting to be started. **/
+    @Override
+    public void init_loop() {
+    }
+
+    /** This method is called once at the start of the OpMode. **/
+    @Override
+    public void start() {
+        //The parameter controls whether the Follower should use break mode on the motors (using it is recommended).
+        //In order to use float mode, add .useBrakeModeInTeleOp(true); to your Drivetrain Constants in Constant.java (for Mecanum)
+        //If you don't pass anything in, it uses the default (false)
+           }
+
+    /** This is the main loop of the opmode and runs continuously after play **/
+    @Override
+    public void loop() {
+        telemetry.update();
+
+        if (gamepad1.yWasPressed()){
+            if (currFrontTargetVelocity == highVelocity){
+                currFrontTargetVelocity = lowVelocity;
+            }
+            else {
+                currFrontTargetVelocity = highVelocity;
+            }
+        }
+        if (gamepad2.yWasPressed()){
+            if (currBackTargetVelocity == highVelocity){
+                currBackTargetVelocity = lowVelocity;
+            }
+            else {
+                currBackTargetVelocity = highVelocity;
+            }
+        }
+
+        // Front
+
+        if(gamepad1.bWasPressed() || gamepad2.bWasPressed()){
+            stepIndex = (stepIndex+1) % stepSizes.length;
+        }
+
+        if (gamepad1.dpadLeftWasPressed()){
+            frontP -= stepSizes[stepIndex];
+        }
+        if (gamepad1.dpadRightWasPressed()){
+            frontP += stepSizes[stepIndex];
+        }
+        if (gamepad1.dpadUpWasPressed()){
+            frontF += stepSizes[stepIndex];
+        }
+        if (gamepad1.dpadDownWasPressed()){
+            frontF -= stepSizes[stepIndex];
+        }
+        if (gamepad1.left_trigger>0){
+            frontI -= stepSizes[stepIndex];
+        }
+        if (gamepad1.right_trigger>0){
+            frontI += stepSizes[stepIndex];
+        }
+        if (gamepad1.leftBumperWasPressed()){
+            frontD -= stepSizes[stepIndex];
+        }
+        if (gamepad1.rightBumperWasPressed()){
+            frontD += stepSizes[stepIndex];
+        }
+
+
+
+        // Back
+
+        if (gamepad2.dpadLeftWasPressed()){
+            backP -= stepSizes[stepIndex];
+        }
+        if (gamepad2.dpadRightWasPressed()){
+            backP += stepSizes[stepIndex];
+        }
+        if (gamepad2.dpadUpWasPressed()){
+            backF += stepSizes[stepIndex];
+        }
+        if (gamepad2.dpadDownWasPressed()){
+            backF -= stepSizes[stepIndex];
+        }
+
+        if (gamepad2.left_trigger>0){
+            backI -= stepSizes[stepIndex];
+        }
+        if (gamepad2.right_trigger>0){
+            backI += stepSizes[stepIndex];
+        }
+        if (gamepad2.leftBumperWasPressed()){
+            backD -= stepSizes[stepIndex];
+        }
+        if (gamepad2.rightBumperWasPressed()){
+            backD += stepSizes[stepIndex];
+        }
+
+        frontPIDF.p = frontP;
+        frontPIDF.f = frontF;
+        frontPIDF.i = frontI;
+        frontPIDF.d = frontD;
+
+        backPIDF.p = backP;
+        backPIDF.f = backF;
+        backPIDF.i = backI;
+        backPIDF.d = backD;
+
+
+        front.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER,frontPIDF);
+        back.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER,backPIDF);
+
+        front.setVelocity(currFrontTargetVelocity);
+        back.setVelocity(currBackTargetVelocity);
+
+        double frontCurrVelocity = front.getVelocity();
+        double backCurrVelocity = back.getVelocity();
+
+        double frontError = currFrontTargetVelocity - frontCurrVelocity;
+        double backError = currBackTargetVelocity - backCurrVelocity;
+
+        telemetry.addData("StepSize : ", stepSizes[stepIndex]);
+        telemetry.addData("Target Velocity : ", targetVelocity);
+        telemetry.addLine("***************************************************");
+        telemetry.addData("Current Front Velocity : ", frontCurrVelocity);
+        telemetry.addData("Current Front Error : ", frontError);
+        telemetry.addLine("-------------------------------------------");
+        telemetry.addData("Current Back Velocity : ", backCurrVelocity);
+        telemetry.addData("Current Back Error : ", backError);
+        telemetry.addLine("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+        telemetry.addData("Tuning Front P : ", "%.4f", frontP);
+        telemetry.addData("Tuning Front I : ", "%.4f", frontI);
+        telemetry.addData("Tuning Front D : ", "%.4f", frontD);
+        telemetry.addData("Tuning Front F : ", "%.4f", frontF);
+        telemetry.addData("Tuning Back P : ", "%.4f", backP);
+        telemetry.addData("Tuning Back I : ", "%.4f", backI);
+        telemetry.addData("Tuning Back D : ", "%.4f", backD);
+        telemetry.addData("Tuning Back F : ", "%.4f", backF);
+    }
+
+    /** We do not use this because everything automatically should disable **/
+    @Override
+    public void stop() {
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/Constants.java
@@ -0,0 +1,87 @@
+package org.firstinspires.ftc.teamcode.pedroPathing.constants;
+
+import com.pedropathing.control.FilteredPIDFCoefficients;
+import com.pedropathing.control.PIDFCoefficients;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.follower.FollowerConstants;
+import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.Encoder;
+import com.pedropathing.ftc.localization.constants.DriveEncoderConstants;
+import com.pedropathing.ftc.localization.constants.PinpointConstants;
+import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.GoBildaPinpointDriver;
+
+public class Constants {
+
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(14.3)
+            .forwardZeroPowerAcceleration(-31.308) // -34.38, -34.19, -29.69, -27.69, -30.59
+            .lateralZeroPowerAcceleration(-68.952) //-69.29 -68.44, 68.47, -68.84, -69.72
+            .useSecondaryTranslationalPIDF(true)
+            .useSecondaryHeadingPIDF(true)
+            .useSecondaryDrivePIDF(true)
+            .centripetalScaling(0.0007)
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.1, 0, 0.01, 0.01))
+            .headingPIDFCoefficients(new PIDFCoefficients(1.25, 0, 0.005, 0.01))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.005, 0, 0.001, 0.6, 0)) // d was 0.0005
+            .secondaryTranslationalPIDFCoefficients(new PIDFCoefficients(0.2, 0, 0.02, 0.01))
+            .secondaryHeadingPIDFCoefficients(new PIDFCoefficients(2.75, 0, 0.003, 0))
+            .secondaryDrivePIDFCoefficients(new FilteredPIDFCoefficients(.05, 0, 0.001, 0.6, 0))// was p: 100, d: 0.001
+            .headingPIDFSwitch(0.07)
+            .translationalPIDFSwitch(10); // 5
+
+
+    public static DriveEncoderConstants robotConstants = new DriveEncoderConstants()
+            .robotWidth(14.2)
+            .robotLength(12.5984);
+
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .leftFrontMotorName("leftFront")
+            .leftRearMotorName("leftRear")
+            .rightFrontMotorName("rightFront")
+            .rightRearMotorName("rightRear")
+            .leftFrontMotorDirection(DcMotorEx.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorEx.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorEx.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorEx.Direction.FORWARD)
+            .xVelocity(62.922) //63.3, 62.9, 63.3, 62.09, 63.02
+            .yVelocity(50.732) //50.73, 50.4, 50.91, 50.66, 50.97
+            .useVoltageCompensation(true)
+            .useBrakeModeInTeleOp(true);
+
+
+
+
+    public static PinpointConstants localizerConstants = new PinpointConstants()
+            .forwardPodY(5)
+            .strafePodX(31.75)
+            .distanceUnit(DistanceUnit.MM)
+            .hardwareMapName("pinpoint")
+            .yawScalar(1.0)
+            .encoderResolution(com.qualcomm.hardware.gobilda.GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
+            .forwardEncoderDirection(com.qualcomm.hardware.gobilda.GoBildaPinpointDriver.EncoderDirection.FORWARD)
+            .strafeEncoderDirection(com.qualcomm.hardware.gobilda.GoBildaPinpointDriver.EncoderDirection.REVERSED);
+
+    public static PathConstraints pathConstraints = new PathConstraints(
+            0.995,
+            50,
+            1.75,
+            15
+    ); // 0.85, 250, 1.75, 15
+
+    public static Follower createFollower(HardwareMap hardwareMap) {
+        return new FollowerBuilder(followerConstants, hardwareMap)
+                .mecanumDrivetrain(driveConstants)
+                .driveEncoderLocalizer(robotConstants)
+                .pinpointLocalizer(localizerConstants)
+                .pathConstraints(pathConstraints)
+                .build();
+    }
+}
+
